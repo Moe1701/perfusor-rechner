23052ea1b49e71d1eb4da4254741f6935f5dffaf
Moe1701 <moeherzog@googlemail.com>
2026-09-25 20:12:57 +0200

    Refactor BaseDataSection to include editable concentration
    Updated BaseDataSection to remove concentration prop and make it directly editable.

--- src/components/calculator/BaseDataSection.jsx
+++ src/components/calculator/BaseDataSection.jsx
@@ -1,22 +1,14 @@
 import { InputGroup } from '../ui/InputGroup';
 
-export const BaseDataSection = ({ baseData, onChange, concentration }) => (
+export const BaseDataSection = ({ baseData, onChange }) => (
     <div className="section">
         <div className="section-title">1. Basisdaten</div>
-        <InputGroup 
-            id="med-mg" label="Wirkstoff" value={baseData.mg} unit="mg" 
-            onChange={(val) => onChange('mg', val)} 
-        />
-        <InputGroup 
-            id="vol-ml" label="Volumen" value={baseData.ml} unit="ml" 
-            onChange={(val) => onChange('ml', val)} 
-        />
-        <InputGroup 
-            id="weight-kg" label="Gewicht" value={baseData.weight} unit="kg" 
-            onChange={(val) => onChange('weight', val)} 
-        />
-        <div className="info-box">
-            Konzentration: {concentration > 0 ? `${concentration.toFixed(1)} µg/ml` : '-- µg/ml'}
+        <InputGroup id="med-mg" label="Wirkstoff" value={baseData.mg} unit="mg" onChange={(val) => onChange('mg', val)} />
+        <InputGroup id="vol-ml" label="Volumen" value={baseData.ml} unit="ml" onChange={(val) => onChange('ml', val)} />
+        <InputGroup id="weight-kg" label="Gewicht" value={baseData.weight} unit="kg" onChange={(val) => onChange('weight', val)} />
+        {/* Die Konzentration ist jetzt ein direkt editierbares Feld! */}
+        <div style={{ marginTop: '12px' }}>
+            <InputGroup id="conc-mcgml" label="Konz." value={baseData.concentration} unit="µg/ml" onChange={(val) => onChange('concentration', val)} highlight />
         </div>
     </div>
 );
