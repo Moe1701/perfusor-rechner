8988d0b3adea18d0806e1dd31e0b2976fd897be0
Moe1701 <moeherzog@googlemail.com>
2026-09-26 17:18:38 +0200

    style(ui): encapsulate base data in tile container, center primary rate inputs, enlarge top units, and rebalance vertical layout towards screen center

--- src/components/calculator/BaseDataSection.jsx
+++ src/components/calculator/BaseDataSection.jsx
@@ -1,13 +1,16 @@
 import { InputGroup } from '../ui/InputGroup';
+import { Tile } from '../ui/Tile';
 
 export const BaseDataSection = ({ baseData, onChange }) => (
     <div className="section">
         <div className="section-title">Basisdaten</div>
-        <InputGroup id="med-mg" label="Wirkstoff" value={baseData.mg} unit="mg" onChange={(val) => onChange('mg', val)} />
-        <InputGroup id="vol-ml" label="Volumen" value={baseData.ml} unit="ml" onChange={(val) => onChange('ml', val)} />
-        <InputGroup id="weight-kg" label="Gewicht" value={baseData.weight} unit="kg" onChange={(val) => onChange('weight', val)} />
-        <div style={{ marginTop: '12px' }}>
-            <InputGroup id="conc-mcgml" label="Konz." value={baseData.concentration} unit="µg/ml" onChange={(val) => onChange('concentration', val)} highlight />
-        </div>
+        <Tile>
+            <InputGroup id="med-mg" label="Wirkstoff" value={baseData.mg} unit="mg" onChange={(val) => onChange('mg', val)} />
+            <InputGroup id="vol-ml" label="Volumen" value={baseData.ml} unit="ml" onChange={(val) => onChange('ml', val)} />
+            <InputGroup id="weight-kg" label="Gewicht" value={baseData.weight} unit="kg" onChange={(val) => onChange('weight', val)} />
+            <div style={{ marginTop: '12px' }}>
+                <InputGroup id="conc-mcgml" label="Konz." value={baseData.concentration} unit="µg/ml" onChange={(val) => onChange('concentration', val)} highlight />
+            </div>
+        </Tile>
     </div>
 );
